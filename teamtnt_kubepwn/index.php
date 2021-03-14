f60d5a22faf168fc4e39cbf3f4af073e4a1b9ff8
Caprico <t>
2021-03-14 13:33:27 -0500

    Team TNT kubepwn

---
+++ teamtnt_kubepwn/index.php
@@ -0,0 +1,11 @@
+ <!DOCTYPE html>
+<html lang="de">
+ <head>
+  <meta charset="UTF-8">
+  <title>Dateibrowser</title>
+ </head>
+<body>
+
+<h3>.</h3><a href="./auto_ip_pwn.sh.txt">auto_ip_pwn.sh.txt</a><br><a href="./auto_kube_shodan.sh.txt">auto_kube_shodan.sh.txt</a><br><a href="./cmd.sh.txt">cmd.sh.txt</a><br><a href="./index.php">index.php</a><br><a href="./spread_kube_loop.sh.txt">spread_kube_loop.sh.txt</a><br>
+</body>
+</html> 
